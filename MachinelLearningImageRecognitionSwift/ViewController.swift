eb268dd10b83df14125e00d46d6d45ad6b4cb221
tahaturan <tahaaturann16@gmail.com>
2023-06-01 15:28:36 +0300

    UI tarafinin olusturulmasi

--- MachinelLearningImageRecognitionSwift/ViewController.swift
+++ MachinelLearningImageRecognitionSwift/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func changeButtonClicked(_ sender: Any) {
+        selectImage()
+      
+        
+    }
+    
+}
 
+
+//MARK: Resim Secim Islemleri
+extension ViewController: UIImagePickerControllerDelegate , UINavigationControllerDelegate{
+    
+    func selectImage() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        
+        picker.sourceType = .photoLibrary
+        
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        
+        self.dismiss(animated: true)
+    }
+    
 }
 
